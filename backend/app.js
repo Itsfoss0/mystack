e8f00f504998993edbaf08987ba10196e7b326bf
Itsfoss0 <com01321@uoeld.ac.ke>
2024-02-08 14:48:24 +0300

    [Chore]: Add backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node 
+
+const express = require('express')
+const logger = require("morgan")
+const cors = require("cors")
+const devRouter = require('./controller/devs')
+const undefinedRoute = require('./middleware/error')
+
+const app = express()
+app.use(logger('tiny'))
+app.use(cors())
+app.use("/api/v1/devs", devRouter)
+app.use(undefinedRoute)
+
+module.exports = app
