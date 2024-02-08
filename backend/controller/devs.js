bbebed8b4b37d242a1a88b096a33b4aa83777baf
Itsfoss0 <com01321@uoeld.ac.ke>
2024-02-08 14:48:49 +0300

    [Chore]: Update dependencies
    Summary:
    Update dependencies to include a logger middlware (morgan)
    With tiny configuration for logging requests in dev

--- backend/controller/devs.js
+++ backend/controller/devs.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-const devRouter = require("express").Router();
-const fs = require("fs");
+const devRouter = require('express').Router();
+const fs = require('fs');
 
-
-devRouter.get("/", (request, response) => {
-  fs.readFile("data.json", "utf-8", (err, data) => {
+devRouter.get('/', (request, response) => {
+  fs.readFile('data.json', 'utf-8', (err, data) => {
     if (err) {
-      console.log("error", err);
+      console.log('error', err);
       return response.status(500).json({ error: 'Internal Server Error' });
     }
     const devsData = JSON.parse(data);
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const app = require("./app");
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Listening on port ", PORT);
+  console.log('Listening on port ', PORT);
 });
